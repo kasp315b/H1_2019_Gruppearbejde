6902293dc05cf76ec287ee8add22b7a0a755819f
Aputsiaq Lindhardt <aputsiaql@gmail.com>
2019-04-15 13:52:01 +0200

    Renamed Bil.cs to Car.cs, and moved/renamed enum with car brands

---
+++ ConsoleAppVersioneringDokumentering/ConsoleAppVersioneringDokumentering/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppVersioneringDokumentering
+{
+    public class Car
+    {
+        public enum CarBrands{ Ford, Volvo, Toyota, Audi, BMW, Suszuki }
+        public int HP { get; private set; }
+        public int KM { get; private set; }
+        public CarBrands Brand { get; private set; }
+        public string Model { get; private set; }
+        public Car()
+        {
+
+        }
+        public Car(string model, CarBrands brand, int hp)
+        {
+            Model = model;
+            Brand = brand;
+            HP = hp;
+        }
+        
+        public void Drive()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Wr");
+            for (int i = 0; i < HP; i++) sb.Append("o");
+            sb.Append("m!");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
