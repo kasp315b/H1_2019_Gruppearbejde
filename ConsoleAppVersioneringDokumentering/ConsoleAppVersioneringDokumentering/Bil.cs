f1e17087c6c98a16751238af8a9b29a3475d6682
Aputsiaq Lindhardt <aputsiaql@gmail.com>
2019-04-15 10:35:12 +0200

    Tilføjet Bil.cs

---
+++ ConsoleAppVersioneringDokumentering/ConsoleAppVersioneringDokumentering/Bil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppVersioneringDokumentering
+{
+    public class Bil
+    {
+        public int HK { get; private set; }
+        public int KM { get; private set; }
+        public string Brand { get; private set; }
+        public string Model { get; private set; }
+        public Bil(string model, string brand, int hk)
+        {
+            Model = model;
+            Brand = brand;
+            HK = hk;
+        }
+    }
+}
