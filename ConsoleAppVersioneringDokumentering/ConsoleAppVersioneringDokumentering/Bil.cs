17e2daf07338b08ea1b0854511e3afc65d50b27a
Aputsiaq Lindhardt <aputsiaql@gmail.com>
2019-04-15 13:15:28 +0200

    Delete BIN Path

--- ConsoleAppVersioneringDokumentering/ConsoleAppVersioneringDokumentering/Bil.cs
+++ ConsoleAppVersioneringDokumentering/ConsoleAppVersioneringDokumentering/Bil.cs
@@ -6,17 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppVersioneringDokumentering
 {
+    public enum BilMaerker { Ford, Volvo, Toyota, Audi, BMW, Suszuki}
     public class Bil
     {
         public int HK { get; private set; }
         public int KM { get; private set; }
-        public string Brand { get; private set; }
+        public BilMaerker Brand { get; private set; }
         public string Model { get; private set; }
-        public Bil(string model, string brand, int hk)
+        public Bil()
+        {
+
+        }
+        public Bil(string model, BilMaerker brand, int hk)
         {
             Model = model;
             Brand = brand;
             HK = hk;
         }
+        
+
     }
 }
