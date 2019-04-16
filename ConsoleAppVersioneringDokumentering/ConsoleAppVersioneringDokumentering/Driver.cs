a71772c6f7e6c009116289c2ce88b59a47c2c11f
Aputsiaq Lindhardt <aputsiaql@gmail.com>
2019-04-16 10:04:33 +0200

    Added Driver.cs

---
+++ ConsoleAppVersioneringDokumentering/ConsoleAppVersioneringDokumentering/Driver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppVersioneringDokumentering
+{
+    public class Driver
+    {
+        public enum Teams { Red_Bull, Ford, Volvo, Toyota, Audi, BMW, Suzuki, Fiat, KIA, Ferrari, Koenigsegg, Porsche, Hyundai }
+
+        public int Age { get; set; }
+        public Car Car { get; set; }
+        public string Name { get; set; }
+        public Teams Team { get; set; }
+    }
+}
