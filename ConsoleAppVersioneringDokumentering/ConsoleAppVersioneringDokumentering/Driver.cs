6c4ae4b2d7c97d49a4c36761896c7ff036220d0f
Aputsiaq Lindhardt <aputsiaql@gmail.com>
2019-04-16 12:15:36 +0200

    fjernet Teams enum og ændret property 'team' til Car.CarBrand

--- ConsoleAppVersioneringDokumentering/ConsoleAppVersioneringDokumentering/Driver.cs
+++ ConsoleAppVersioneringDokumentering/ConsoleAppVersioneringDokumentering/Driver.cs
@@ -8,11 +8,9 @@ namespace ConsoleAppVersioneringDokumentering
 {
     public class Driver
     {
-        public enum Teams { Red_Bull, Ford, Volvo, Toyota, Audi, BMW, Suzuki, Fiat, KIA, Ferrari, Koenigsegg, Porsche, Hyundai }
-
         public int Age { get; set; }
         public Car Car { get; set; }
         public string Name { get; set; }
-        public Teams Team { get; set; }
+        public Car.CarBrand Team { get; set; }
     }
 }
